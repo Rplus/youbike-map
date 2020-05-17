ceffac131356158a90fcd994c4fd256d0c8b9693
Rplus <rplus.tw@gmail.com>
2020-05-17 20:24:11 +0800

    done

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,208 @@
 <script>
 	let name = 'world';
+  let range = 500;
+  let location = null;
+  let points = [];
+  $: recentPoints = getRecentPoints(points, range, location);
+  const KEY = 'AIzaSyCjDnDGv67nvhzBsLRYAwTbiF1HrZBQDUM';
+
+  $: currentPosMarker = location ? `&markers=${location.lat},${location.lng}` : undefined;
+
+  const locateOption = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  function getLocation () {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject, locateOption);
+    });
+  };
+
+  async function setLocate() {
+    let position = await getLocation();
+    location = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+      acc: position.coords.accuracy,
+    };
+    console.log('setLocate', location);
+  }
+
+  async function fetchData() {
+    setLocate();
+
+    console.log('fetchData');
+    const res = await fetch('https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json').then(d => d.json())
+    console.log('fetchData done', res);
+    points = Object.values(res.retVal);
+  };
+
+  function getRecentPoints(points, range, location) {
+    console.log('getRecentPoints');
+    points = points.map(p => {
+      p.img = getImgSrc(p);
+      return p;
+    });
+
+    if (location) {
+      points = points.filter(p => {
+        const distance = GreatCircleDistance([
+          location.lng,
+          location.lat,
+          p.lng,
+          p.lat
+        ]);
+        p.distance = distance.toFixed();
+        return distance < range;
+      })
+    }
+
+    return points.sort((a, b) => a.distance - b.distance);
+  }
+
+  // https://en.wikipedia.org/wiki/Great-circle_distance
+  // https://gist.github.com/thesadabc/f84adeea5644149539dae968ccdb3f2c
+  function GreatCircleDistance(points) {
+    let [lng1, lat1, lng2, lat2] = points;
+    if (!lng1 || !lat1 || !lng2 || !lat2) return null;
+    [lng1, lat1, lng2, lat2] = points.map(n => n * Math.PI / 180)
+    const R = 6371000; // average earth radius(m)
+    const C = Math.sin(lat1) * Math.sin(lat2) +
+      Math.cos(lng1 - lng2) * Math.cos(lat1) * Math.cos(lat2);
+    return R * Math.acos(C);
+  }
+
+  function getImgSrc(
+    target,
+    size = 300,
+  ) {
+    let p0 = target;
+    let p1;
+
+    if (location) {
+      p0 = location;
+      p1 = target;
+    }
+
+    return (
+      'https://maps.googleapis.com/maps/api/staticmap'
+      + `?size=${size}x${size}`
+      + `&center=${p0.lat},${p0.lng}`
+      + `&markers=size:mid|${p0.lat},${p0.lng}`
+      + (p1 ? `&markers=color:yellow|label:T|${p1.lat},${p1.lng}` : '')
+      + `&key=${KEY}`
+    );
+  }
+
+  fetchData();
 </script>
 
-<button id="locate">locate</button>
-<button id="refetch">refetch</button>
-<input type="range" id="range"/>
-<ul>
-  <li></li>
-</ul>
+
+
+<!--  -->
+
+
+
+<nav class="ctrl">
+  <button id="locate" on:click={setLocate}>Locate</button>
+  <button id="refetch" on:click={fetchData}>Refetch</button>
+  <input type="range" id="range" bind:value={range} max="5000" min="10" />
+  <span>Range: {range}m</span>
+</nav>
+
+<ol class="list">
+  {#each recentPoints as point (point.sno)}
+    <li class="item" value={point.sno}>
+      <div class="d-f">
+        <div>
+          車：{point.sbi}
+          <br>
+          位：{point.bemp}
+          <hr>
+          <small>共：{point.tot}</small>
+        </div>
+
+        <div class="item-info">
+          {point.sna}
+          <br>
+          &nbsp;&nbsp;&nbsp;&nbsp;{point.distance} m
+          <br>
+          <br>
+          <details>
+            <a href="http://maps.google.com?q={point.lat},{point.lng}" target="_blank" title="google map link">{point.lat}, {point.lng}
+            </a>
+            <a class="img" style="--url: url({point.img})" href={point.img} target="_blank">{point.img}</a>
+          </details>
+
+        </div>
+
+      </div>
+    </li>
+  {/each}
+</ol>
+
+
+
+<!--  -->
+
+
+
+<style>
+  .ctrl {
+    position: fixed;
+    top: 0;
+    height: 2em;
+    display: flex;
+    align-items: center;
+  }
+
+  .list {
+    padding-top: 2em;
+  }
+
+  .data-number {
+    display: inline-block;
+    width: 100px;
+    text-align: center;
+  }
+
+  .item {
+    margin-bottom: 1.5em;
+  }
+
+  .item-info {
+    margin-left: 2em;
+    font-size: smaller;
+  }
+
+  .img {
+    display: block;
+    width: 300px;
+    height: 300px;
+    word-break: break-all;
+    color: transparent;
+  }
+
+  :global(details[open]) .img {
+    background-color: #cfc;
+    background-image: var(--url);
+  }
+
+  .sr-only {
+    position: absolute !important;
+    width: 1px !important;
+    height: 1px !important;
+    padding: 0 !important;
+    margin: -1px !important;
+    overflow: hidden !important;
+    clip: rect(0, 0, 0, 0) !important;
+    white-space: nowrap !important;
+    border: 0 !important;
+  }
+
+  .d-f {
+    display: flex;
+  }
+</style>
